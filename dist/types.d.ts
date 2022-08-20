1a65e2c2ac4cf0fa20becd625a35d7f6268684b0
zakiAzfar <dexk2015@gmail.com>
2022-08-20 18:29:36 +0530

    added uglify script

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,17 +1,15 @@
 type DeviceListType = {
-  type: string;
-  udid: string;
-  port: string;
+    type: string;
+    udid: string;
+    port: string;
 }[];
 declare const adb: {
-  isAdbInstalled: () => Promise<boolean>;
-  reconnect: () => Promise<string>;
-  startServer: () => Promise<string>;
-  killServer: () => Promise<string>;
-  setAdbExplicitPath: (path: string) => void;
-  setCurrentActiveDevice: (device: string) => void;
-  deviceList: () => Promise<DeviceListType>;
+    isAdbInstalled: () => Promise<boolean>;
+    reconnect: () => Promise<string>;
+    startServer: () => Promise<string>;
+    killServer: () => Promise<string>;
+    setAdbExplicitPath: (path: string) => void;
+    setCurrentActiveDevice: (device: string) => void;
+    deviceList: () => Promise<DeviceListType>;
 };
 export default adb;
-
-//# sourceMappingURL=types.d.ts.map
