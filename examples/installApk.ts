366ab77b049614b247eca755d1829e3205b42043
amiralam0 <sharpprogrammer2018@gmail.com>
2022-08-20 07:47:42 -0700

    added new methods

--- examples/installApk.ts
+++ examples/installApk.ts
@@ -3,7 +3,7 @@ import adb from '../src/adb';
 async function main(){
     adb.setCurrentActiveDevice((await adb.deviceList())[0].udid);
     // console.log(await adb.uninstallApp("io.testproject.demo"))
-    console.log(await adb.installApp("C:\\Users\\user\\Downloads\\testproject-demo-app.apk"))
+    // console.log(await adb.installApp("C:\\Users\\user\\Downloads\\testproject-demo-app.apk"))
 
 }
 
