676a2417c03007452e9f69dab5c40d72712a392e
amiralam0 <sharpprogrammer2018@gmail.com>
2022-08-20 07:45:52 -0700

    added install and uninstall apk methods

---
+++ examples/installApk.ts
@@ -0,0 +1,10 @@
+import adb from '../src/adb';
+
+async function main(){
+    adb.setCurrentActiveDevice((await adb.deviceList())[0].udid);
+    // console.log(await adb.uninstallApp("io.testproject.demo"))
+    console.log(await adb.installApp("C:\\Users\\user\\Downloads\\testproject-demo-app.apk"))
+
+}
+
+main()
