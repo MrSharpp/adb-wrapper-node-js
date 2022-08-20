03c51c16eab4689e28772eebb2b9d7e07fcd8d02
amiralam0 <sharpprogrammer2018@gmail.com>
2022-08-20 09:32:49 -0700

    added shell execution , current opended activity

--- examples/installApk.ts
+++ examples/installApk.ts
@@ -2,7 +2,7 @@ import adb from "../src/adb";
 
 async function main() {
   adb.setCurrentActiveDevice((await adb.deviceList())[0].udid);
-  console.log(await adb.getCurrentOpendedActivity())
+  console.log(await adb.getCurrentOpendedActivity());
   // console.log(await adb.uninstallApp("io.testproject.demo"))
   // console.log(await adb.installApp("C:\\Users\\alam\\Downloads\\testproject-demo-app.apk"))
 }
